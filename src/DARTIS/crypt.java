1d63ba33047ade9ace05cd224374e0a77e2ef2af
PDGLOBAL <ZACH@PDGLOBAL.NET>
2018-03-09 19:29:44 -0500

    Compacted/optimized
    Compacted some lines, removed commented out debug lines, small
    optimizations in encryption error correction.

--- src/DARTIS/crypt.java
+++ src/DARTIS/crypt.java
@@ -6,44 +6,28 @@ import Jama.Matrix;
 
 public class crypt {
 
-	
 	public static String inject(String data, String[] key) {
 		String[] temp = strings.stringequalsplit(data, 1);
 		double[] insert[] = new double[temp.length/9+1][10];
 		int n = 0; int l = 0;
 		for (int i = 1; i<=temp.length-1; i++) {
-			 int ascii = temp[i].charAt(0);
-			insert[l][n] = ascii;
+			insert[l][n] = temp[i].charAt(0);
 			n += 1;
-			if(n > 9) {
-				n = 0;
-				l += 1;
-				}
+			if(n > 9) { n = 0; l += 1; }
 			}
-			while(n < 9) {
-			insert[l][n] = 1;
-			n += 1;
-			}
+			while(n < 9) { insert[l][n] = 1;n += 1; }
 			String[] timestamp = time.getTimestamp(key.length);
 					Matrix matrix = new Matrix(insert);
 					Matrix nextLayer = construct.hologram(key[Integer.parseInt(timestamp[1])]);
 					matrix = matrix.times(nextLayer);
 					String id = timestamp[1]+";";
 					for(int i=2;i<=Integer.parseInt(timestamp[0]);i++) {
-						if (Integer.parseInt(timestamp[i]) < 1) {
-							timestamp[i] = "1";
-						}
+						if (Integer.parseInt(timestamp[i]) < 1) { timestamp[i] = "1"; }
 							id = id+ timestamp[i]+";";
-							//System.out.println("i="+i+"="+timestamp[i]);
-					}
-					for(int i=2;i<=Integer.parseInt(timestamp[0]);i++) {
-						if (Integer.parseInt(timestamp[i]) < 1) {
-							timestamp[i] = "1";
-						}
 							matrix = matrix.times(construct.hologram(key[Integer.parseInt(timestamp[i])]));
-							//System.out.println(timestamp[i]);
 					}
-		String verify = crypt.extract(strings.array2str(matrix.getArray()).replace(" ", "").replace("[","").replace("]", "").replace(", ", ",")+"~"+id, key);
+		String ret = strings.array2str(matrix.getArray()).replace(" ", "").replace("[","").replace("]", "").replace(", ", ",")+"~"+id;
+		String verify = crypt.extract(ret, key);
 		while(verify.equals(data) == false) {
 			timestamp = time.getTimestamp(key.length);
 			matrix = new Matrix(insert);
@@ -51,22 +35,14 @@ public class crypt {
 			matrix = matrix.times(nextLayer);
 			id = timestamp[1]+";";
 			for(int i=2;i<=Integer.parseInt(timestamp[0]);i++) {
-				if (Integer.parseInt(timestamp[i]) < 1) {
-					timestamp[i] = "1";
-				}
+				if (Integer.parseInt(timestamp[i]) < 1) { timestamp[i] = "1"; }
 					id = id+ timestamp[i]+";";
-					//System.out.println("i="+i+"="+timestamp[i]);
-			}
-			for(int i=2;i<=Integer.parseInt(timestamp[0]);i++) {
-				if (Integer.parseInt(timestamp[i]) < 1) {
-					timestamp[i] = "1";
-				}
 					matrix = matrix.times(construct.hologram(key[Integer.parseInt(timestamp[i])]));
-					//System.out.println(timestamp[i]);
 			}
-        verify = crypt.extract(strings.array2str(matrix.getArray()).replace(" ", "").replace("[","").replace("]", "").replace(", ", ",")+"~"+id, key);
+			ret = strings.array2str(matrix.getArray()).replace(" ", "").replace("[","").replace("]", "").replace(", ", ",")+"~"+id;
+			verify = crypt.extract(ret, key);
 		}
-		return strings.array2str(matrix.getArray()).replace(" ", "").replace("[","").replace("]", "").replace(", ", ",")+"~"+id;
+        return ret;
 	}
 	
 	public static String extract(String data, String[] key) {
@@ -74,7 +50,6 @@ public class crypt {
 		Matrix film = construct.hologram(properties[0]);
 		String[] indexList = properties[1].replace(" ", "").replace("[","").replace("]", "").split(";");
 		for (int i=0;i<=indexList.length-1;i++) {
-			//System.out.println("Inverting Matrix index: "+(indexList.length-1-i) +"="+Integer.parseInt(indexList[(indexList.length-1-i)]));
 			film = film.times(construct.hologram(key[Integer.parseInt(indexList[(indexList.length-1-i)])]).inverse());
 		}
 		double[][] original = film.getArray();
@@ -85,11 +60,9 @@ public class crypt {
 			int chrvalue = internalMath.safeLongToInt(Math.round(Double.parseDouble(line[i])));
 			if (chrvalue > 1.1 &&  chrvalue < 500000) {
 				String chr = String.valueOf(Character.toChars(chrvalue));
-			    //System.out.println("Index "+i+" of "+(Array.getLength(line)-1)+" encoded as "+line[i]+" Decoded to "+ chr);
 				ascii += chr;
 			}
 		}
 		return ascii;
-	}
-	
+	}	
 }
