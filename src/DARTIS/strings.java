cd6bc60bf12a1e4c3d388b9665c915e0c8cfbbf5
Administrator <Administrator@CLOUD.intranet.pdglobal.net>
2018-04-10 15:48:22 -0400

    More formatting revisions...

--- src/DARTIS/strings.java
+++ src/DARTIS/strings.java
@@ -4,36 +4,37 @@ import java.util.Arrays;
 
 public class strings {
 
-	public static String[] stringequalsplit(String text,int length) {
+	public static String[] stringequalsplit(String text, int length) {
 		int arraysize = 0;
 		if (length < 2) {
-			arraysize = text.length()+1;
+			arraysize = text.length() + 1;
 		} else {
-			arraysize = Math.round(text.length()/length)+(length/2);
+			arraysize = Math.round(text.length() / length) + (length / 2);
 		}
 		String[] list = new String[arraysize];
-		//System.out.println("Array size = "+arraysize);
+		// System.out.println("Array size = "+arraysize);
 		int index = 0;
 		int realindex = 1;
-		while (index<text.length()) {
-		    list[realindex] = text.substring(index, Math.min(index+length,text.length()));
-		    //System.out.println(index+" - "+Math.min(index+length,text.length())+" = "+text.substring(index, Math.min(index+length,text.length())));
-		    index=index+length;
-		    realindex+=1;
+		while (index < text.length()) {
+			list[realindex] = text.substring(index, Math.min(index + length, text.length()));
+			// System.out.println(index+" - "+Math.min(index+length,text.length())+" =
+			// "+text.substring(index, Math.min(index+length,text.length())));
+			index = index + length;
+			realindex += 1;
 		}
-		list[0] = String.valueOf(realindex-1);
-	return list;
-	}
-	
-	public static String array2str(double[] array[]) {
-	String lineSeparator = ";";
-	StringBuilder sb = new StringBuilder();
-
-	for (double[] row : array) {
-	    sb.append(Arrays.toString(row).replace(" ", "").replace("[","").replace("]", "")+lineSeparator);
+		list[0] = String.valueOf(realindex - 1);
+		return list;
 	}
 
-	String result = sb.toString();
-	return result;
+	public static String array2str(double array[][]) {
+		String lineSeparator = ";";
+		StringBuilder sb = new StringBuilder();
+
+		for (double[] row : array) {
+			sb.append(Arrays.toString(row).replace(" ", "").replace("[", "").replace("]", "") + lineSeparator);
+		}
+
+		String result = sb.toString();
+		return result;
 	}
 }
