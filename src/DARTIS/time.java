f0f54bb12b194efe60479ae43f64741afaf9d48a
Administrator <Administrator@CLOUD.intranet.pdglobal.net>
2018-04-10 16:58:01 -0400

    Increased rate of change for the timestamp. Salted timestamp with rand. Formatting improvements.

--- src/DARTIS/time.java
+++ src/DARTIS/time.java
@@ -1,17 +1,19 @@
 package DARTIS;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Calendar;
+import java.util.Random;
 
 public class time {
 	public static String[] getTimestamp(int length) {
-		long estimatedTime = System.nanoTime();
+		float estimatedTime = (float) System.nanoTime();
 		LocalDate today = LocalDate.now();
 		int dateFormatter = today.getYear();
 		LocalTime time = LocalTime.now();
 		int clock = Integer.parseInt(time.toString().replace(":", "").replace(".", ""));
-		String datetime = String.valueOf((Calendar.DAY_OF_YEAR + dateFormatter + clock));
+		String datetime = String.valueOf((Calendar.DAY_OF_YEAR * dateFormatter * clock));
 		String datetime_era = String.valueOf((Calendar.ERA));
 		String s = datetime_era;
 		byte[] bytes = s.getBytes();
@@ -27,6 +29,10 @@ public class time {
 		// "+String.valueOf(datetime)+String.valueOf(binary)+Long.toString(estimatedTime));
 		String[] ERA = strings.stringequalsplit(String.valueOf(binary), 4);
 		int ERACompressed = Integer.parseInt(ERA[1]) + Integer.parseInt(ERA[2]);
-		return strings.stringequalsplit(datetime + ERACompressed + estimatedTime, String.valueOf(length).length() - 1);
+		Random rand = new Random();
+		String ret = String.valueOf(
+				new BigDecimal(Math.abs(((Float.valueOf(datetime) * 100.0f) * (ERACompressed * estimatedTime) * 100.0f)
+						/ (rand.nextFloat() * 100.0f) * 100.0f)));
+		return strings.stringequalsplit(ret, String.valueOf(length).length() - 1);
 	}
 }
