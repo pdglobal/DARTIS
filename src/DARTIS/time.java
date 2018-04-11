8b064811c6537c5c1bbcdc98e72e1eb51f1b417f
Administrator <Administrator@CLOUD.intranet.pdglobal.net>
2018-04-10 22:09:34 -0400

    Comments

--- src/DARTIS/time.java
+++ src/DARTIS/time.java
@@ -12,7 +12,7 @@ public class time {
 			counter = 0;
 		}
 		if (estimatedTime.toString().substring(0, 1).equals("-")) {
-			estimatedTime = new BigDecimal(new StringBuilder(estimatedTime.abs().toPlainString()).reverse().toString());
+			estimatedTime = new BigDecimal(new StringBuilder(estimatedTime.abs().toPlainString()).reverse().toString()); //I know this isn't anywhere near an optimal solution, looking for any suggestions on a better way to handle this
 		}
 		return strings.stringequalsplit((estimatedTime).toPlainString().replace(".", ""),
 				String.valueOf(length).length() - 1);
