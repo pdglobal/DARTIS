724db8b81433c1bfaa6d685fab555ae33325b62b
Administrator <Administrator@CLOUD.intranet.pdglobal.net>
2018-04-11 00:43:21 -0400

    Formatting revision.

--- src/DARTIS/time.java
+++ src/DARTIS/time.java
@@ -13,9 +13,12 @@ public class time {
 			counter = 0;
 		}
 		if (estimatedTime.toString().substring(0, 1).equals("-")) {
-			estimatedTime = new BigDecimal(new StringBuilder(estimatedTime.abs().toPlainString()).reverse().toString()); //I know this isn't anywhere near an optimal solution, looking for any suggestions on a better way to handle this
+			estimatedTime = new BigDecimal(new StringBuilder(estimatedTime.abs().toPlainString()).reverse().toString());
+			// I know this isn't anywhere near an optimal solution, looking for any
+			// suggestions on a better way to handle this
 		}
-		String baseData = (estimatedTime).toPlainString().replace(".", "").concat(String.valueOf(runtime.freeMemory()).replace(".", ""));
+		String baseData = (estimatedTime).toPlainString().replace(".", "")
+				.concat(String.valueOf(runtime.freeMemory()).replace(".", ""));
 		String timestamp[] = strings.stringequalsplit(baseData, String.valueOf(length).length() - 1);
 		return timestamp;
 	}
