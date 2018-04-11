7ae9d042816e0be3c2b9924a2cc7c1a13cc53cfc
Administrator <Administrator@CLOUD.intranet.pdglobal.net>
2018-04-10 21:45:44 -0400

    Removed extra data from time string. Added in custom counter to offset time measurements.

--- src/DARTIS/time.java
+++ src/DARTIS/time.java
@@ -3,24 +3,11 @@ package DARTIS;
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.Calendar;
-import java.util.Random;
-
 public class time {
+	public static int counter = 0;
 	public static String[] getTimestamp(int length) {
-		float estimatedTime = (float) System.nanoTime();
-		LocalDate today = LocalDate.now();
-		int dateFormatter = today.getYear();
-		LocalTime time = LocalTime.now();
-		int clock = internalMath.compute(time.toString().replace(":", "+"));
-		String datetime = String.valueOf(Calendar.DAY_OF_YEAR + dateFormatter) + clock;
-		Random rand = new Random();
-		MathContext mc = new MathContext(23, RoundingMode.HALF_UP);
-		String ret = new BigDecimal(datetime.concat(String.valueOf(estimatedTime).replace(".", "").replace("-", "")))
-				.divide(new BigDecimal(rand.nextDouble() * 100.0f), mc).toPlainString().replace(".", "")
-				.replaceAll("-", "");
-		return strings.stringequalsplit(ret.substring(0,23), String.valueOf(length).length() - 1);
+		counter += 1;
+		BigDecimal estimatedTime = new BigDecimal(System.nanoTime()).add(new BigDecimal(counter));
+		return strings.stringequalsplit(String.valueOf(estimatedTime).replace(".", ""), String.valueOf(length).length() - 1);
 	}
 }
