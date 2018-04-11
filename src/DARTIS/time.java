f353cf58f2ad3e21f5c7acb948d55a9f32ccacf7
Administrator <Administrator@CLOUD.intranet.pdglobal.net>
2018-04-10 22:07:17 -0400

    Formatting.

--- src/DARTIS/time.java
+++ src/DARTIS/time.java
@@ -1,15 +1,20 @@
 package DARTIS;
 
 import java.math.BigDecimal;
-import java.math.MathContext;
-import java.math.RoundingMode;
+
 public class time {
 	public static int counter = 0;
+
 	public static String[] getTimestamp(int length) {
 		counter += 1;
 		BigDecimal estimatedTime = new BigDecimal(System.nanoTime()).add(new BigDecimal(counter));
-		if (counter > 1000) { counter = 0; }
-		if (estimatedTime.toString().substring(0, 1).equals("-")) { estimatedTime = new BigDecimal(new StringBuilder(estimatedTime.abs().toPlainString()).reverse().toString()); }
-		return strings.stringequalsplit((estimatedTime).toPlainString().replace(".", ""), String.valueOf(length).length() - 1);
+		if (counter > 1000) {
+			counter = 0;
+		}
+		if (estimatedTime.toString().substring(0, 1).equals("-")) {
+			estimatedTime = new BigDecimal(new StringBuilder(estimatedTime.abs().toPlainString()).reverse().toString());
+		}
+		return strings.stringequalsplit((estimatedTime).toPlainString().replace(".", ""),
+				String.valueOf(length).length() - 1);
 	}
 }
