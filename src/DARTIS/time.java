8e8de99d3af790f634adb1c43ad70a72192f1bab
Administrator <Administrator@CLOUD.intranet.pdglobal.net>
2018-04-10 21:53:26 -0400

    Offset counter reset added so it doesn't grow out of control.

--- src/DARTIS/time.java
+++ src/DARTIS/time.java
@@ -8,6 +8,7 @@ public class time {
 	public static String[] getTimestamp(int length) {
 		counter += 1;
 		BigDecimal estimatedTime = new BigDecimal(System.nanoTime()).add(new BigDecimal(counter));
+		if (counter > 1000) { counter = 0; }
 		return strings.stringequalsplit(String.valueOf(estimatedTime).replace(".", ""), String.valueOf(length).length() - 1);
 	}
 }
